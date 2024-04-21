c0c128d92021a99e9c46fa21cc0e87021674f7b2
Felilpz <luizfelipe.vieira12@gmail.com>
2024-04-21 01:14:38 -0300

    wip

--- README.old.md
+++
@@ -1,6 +0,0 @@
-# login-react
-
-Minha primeira tela de login funcional.
-
-arquivo em js vanilla
-arquivo em react!
---
+++ api/controllers/user.js
@@ -0,0 +1,11 @@
+import { db } from "../db";
+
+export const getUsers = (_, res) => {
+    const consulta = "select * from usuarios";
+
+    db.query(consulta, (err, data) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json(data);
+    });
+};
